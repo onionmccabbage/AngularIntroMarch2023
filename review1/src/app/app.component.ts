a0b9ab74a0dad6954f9721be70b78a48d9337eed
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-20 14:45:43 +0000

    sample solution to review exercise 1

---
+++ review1/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  fbName = 'billgates'
+  fbUrl = 'https://graph.facebook.com/billgates/picture?type=normal'
+  updateName(evt:any){
+    this.fbName = evt.target.value
+    this.fbUrl = `https://graph.facebook.com/${this.fbName}/picture?type=normal`
+  }
+}
