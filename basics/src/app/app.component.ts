5e86228d78a7e7ecf18329cf2ad8a19f0182e962
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-20 12:30:22 +0000

    lunch day 1

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: '<h3>Coffee Time</h3>'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // here are properties of this class
+  // we model our data as class properties
+  
+  // ES has no notion of private, public
+  // BUT we can control scope  
   title = 'basics';
+  today = new Date()
+  products = [
+    {'name':'Pots', 'price':3.99},
+    {'name':'Dots', 'price':5.99},
+    {'name':'Spots', 'price':1.99}
+  ]
+  showFlag = true
+  imgUrl = 'https://placehold.co/120x64'
+  // here are methods of this class
+  doStuff(){
+    console.log('button was clicked')
+  }
+
+
+
 }
--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTopBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
