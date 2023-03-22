cec9733a339f80e8168aa1ddeed19a735847b9b2
onionmccabbage <onionmccabbage@live.com>
2023-03-22 12:24:46 +0000

    observable demo

---
+++ demo_obs/main.js
@@ -0,0 +1,15 @@
+// grab the 'search' field
+const search = document.getElementById('search')
+const isNum = (code)=>{
+    return 32 && code <=126
+}
+
+// by convention observables have a trailing $ 
+const keyup$ = Rx.Observable.fromEvent(document, 'keyup')
+
+// here is a subscriber
+sub1 = keyup$.pluck('keyCode')
+    .filter( isNum ) // only respond 32-126
+    .subscribe( (k)=>{
+        console.log(`The user typed ${String.fromCharCode(k)}`)
+    } )
